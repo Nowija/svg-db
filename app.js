1c5dfd007f22851af9d30995487dfe66c5d0167a
Nowija <04jano@gmail.com>
2021-07-04 20:38:05 +0200

    Modified the way svgs are loaded

--- app.js
+++ app.js
@@ -7,79 +7,32 @@ const port = 3001;
 const handlebars = require('express-handlebars');
 
 
-// Svgs
+// Reading all svg-folders
 //####################################
-var svgNames = fs.readdirSync('public/');
+var svgDirs = fs.readdirSync('public/');
 var svgList = [];
 
-for(var i=0; i<svgNames.length; i++){
+for(var i=0; i<svgDirs.length; i++){
     svgList.push(
         {
-            name: svgNames[i],
-            id: svgNames[i].replace('.svg', ''),
-            number: i+1,
-            code: fs.readFileSync(`public/${svgNames[i]}`)
+            directory: svgDirs[i],
+            svgs: []
         }
     );
-}
-
-console.log(`Svgs loaded: ${svgList.length}`);
-
-
-// Colors
-//####################################
-const colors = [
-    {
-        name: 'Color-picker',
-        testing: [
-            "#262D37",
-            "#39414E",
-            "#59D4DB",
-            "#599DDB",
-            "#5AD9A4",
-            "#27AE60",
-            "#FF4758",
-            "#FF2539"
-        ]
-    },
-    {
-        name: 'Fireship',
-        testing: [
-            "#0F0E17",
-            "#85FD78",
-            "#75F661",
-            "#46ACDC",
-            "#1692CD",
-            "#FE70DC",
-            "#FE53F8",
-            "#FEE90C",
-            "#FF9F4F",
-            "#FD7273"
-        ]
-    },
-    {
-        name: 'Figma',
-        testing: [
-            "#333333",
-            "#4f4f4f",
-            "#828282",
-            "#bdbdbd",
-            "#e0e0e0", 
-            "#f2f2f2",
-            "#eb5757",
-            "#f2994a",
-            "#f2c94c",
-            "#219653",
-            "#27ae60",
-            "#6fcf97",
-            "#2f80ed",
-            "#2d9cdb",
-            "#56ccf2",
-            "#9b51e0",
-            "#bb6bd9"
-        ]
+    var currentSvg = fs.readdirSync(`public/${svgDirs[i]}`);
+    for(var n=0; n<currentSvg.length; n++){
+        var currentSvgCode = fs.readFileSync(`public/${svgDirs[i]}/${currentSvg[n]}`)
+        svgList[i].svgs.push(
+            {
+                name: currentSvg[n],
+                code: currentSvgCode
+            }
+        );
     }
-];
+};
+
+console.log(`Svg folders loaded:\n${svgDirs}`);
+
 
 // Add the "styles" folder
 //####################################
