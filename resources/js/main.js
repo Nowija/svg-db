a4a8d0d0c383f17543f68080936f4a8c47cab964
Nowija <04jano@gmail.com>
2021-07-05 12:49:46 +0200

    Svgs now has random colors

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,11 +1,28 @@
-$(document).ready(function(){
-    $.get('http://localhost:3001/load/svgs', function(svgList){
-        for(var i=0; i<svgList.length; i++){
-            $("main").append(`<h2>${svgList[i].directory}</h2>`);
+$.get('http://localhost:3001/load/svgs', function(svgList){
+    for(var i=0; i<svgList.length; i++){
+        $("main").append(`<h2>${svgList[i].directory}</h2>`);
 
-            for(var n=0; n<svgList[i].svgs.length; n++){
-                $("main").append(svgList[i].svgs[n].code);
-            }
-        };
-    });
+        for(var n=0; n<svgList[i].svgs.length; n++){
+            const colors = [
+                '#35bf2f',
+                '#5979da',
+                '#f71828',
+                '#fc5b1f',
+                '#be42ff',
+                '#be4c57',
+                '#360cd9',
+                '#34b0f0'
+            ];
+            var color = colors[Math.floor(Math.random()*colors.length)];
+            $("main").append(`
+                <a style="color:${color};">
+                    ${svgList[i].svgs[n].code}
+                </a>
+            `);
+        }
+    };
+
+    // $("svg").click(function(){
+    //     console.log($(this)[0].outerHTML);
+    // });
 });
