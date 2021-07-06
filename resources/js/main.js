943ed6a3594dc253c3eeb85fbadcba98762055d8
Nowija <04jano@gmail.com>
2021-07-06 09:35:09 +0200

    Added and styled popupwindow

--- resources/js/main.js
+++ resources/js/main.js
@@ -34,7 +34,23 @@ $.get('http://localhost:3001/load/svgs', function(svgList){
         `);
     }
 
-    // $("svg").click(function(){
-    //     console.log($(this)[0].outerHTML);
-    // });
+    $("svg").click(function(){
+        togglePopup($(this)[0]);
+    });
 });
+
+function togglePopup(code){
+    // Visar / gömmer popup
+    $(".popup").toggle();
+    // Tar bort förra svg:n
+    $(".svgContainer").children().remove();
+    // Hämtar nuvarande färgen på svg:n
+    // och sätter den på svg:n i popup:en
+    var svgColor = code.parentElement.style.color;
+    $(".svgContainer").append(code.outerHTML).css("color", svgColor);
+    // Visar svg:ns namn och kod i popupen
+    $("#svgName").html('svg-placeholdername.svg').css("color", svgColor);
+    $("#svgCode").html(code.outerHTML);
+}
+
+// https://randomcolorgenerator.net/
