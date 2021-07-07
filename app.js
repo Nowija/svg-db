355a215521331e7265ec2153d4d73933732dfd41
Nowija <04jano@gmail.com>
2021-07-07 13:47:49 +0200

    Files inside the pulic-folder now dosen't crash the app

--- app.js
+++ app.js
@@ -9,32 +9,40 @@ const handlebars = require('express-handlebars');
 
 // Reading all svg-folders
 //####################################
-var svgDirs = fs.readdirSync('public/');
+function isDirectory(path) {  
+    const stats = fs.statSync(path)
+    return stats.isDirectory()
+}
+
+var public = fs.readdirSync(`public/`);
+var svgDirs = [];
 var svgList = [];
+
+for(var i=0; i<public.length; i++){
+    if(isDirectory(`public/${public[i]}`)){
+        svgDirs.push(`public/${public[i]}`);
+    }
+}
 
 if(svgDirs.length == 0){
     console.log('There are no folders and svgs to load!');
-} else {    
-    for(var i=0; i<svgDirs.length; i++){
-        svgList.push(
-            {
-                directory: svgDirs[i],
-                svgs: []
-            }
-        );
-        var currentSvg = fs.readdirSync(`public/${svgDirs[i]}`);
+} else {
+    for(var i=0; i<svgDirs.length; i++){        
+        svgList.push({
+            directory: svgDirs[i].slice(7),
+            svgs: []
+        });
+        var currentSvg = fs.readdirSync(svgDirs[i]);
         for(var n=0; n<currentSvg.length; n++){
-            var currentSvgCode = fs.readFileSync(`public/${svgDirs[i]}/${currentSvg[n]}`, 'utf-8');
-            svgList[i].svgs.push(
-                {
-                    name: currentSvg[n],
-                    code: currentSvgCode
-                }
-            );
+            var currentSvgCode = fs.readFileSync(`${svgDirs[i]}/${currentSvg[n]}`, 'utf-8');
+            svgList[i].svgs.push({
+                name: currentSvg[n],
+                code: currentSvgCode
+            });
         }
     };
 
-    console.log(`Loaded svg-folders: ${svgDirs}`);
+    console.log(svgDirs)
 }
 
 
