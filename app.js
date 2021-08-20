20b784d7cc96a6012366bfa0ccf92d90dcd9446f
Nowija <04jano@gmail.com>
2021-08-20 14:47:51 +0200

    Moved from hbs to ejs and made the webpage responsive

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3001;
-const handlebars = require('express-handlebars');
 
 
 // Reading all svg-folders
@@ -46,21 +45,14 @@ if(svgDirs.length == 0){
 }
 
 
+// Setting up ejs
+//####################################
+app.set('view engine', 'ejs');
+
+
 // Adding project-folders
 //####################################
 app.use(express.static('resources/'));
-
-
-// Setting up handlebars
-//####################################
-app.set('view engine', 'hbs');
-
-app.engine('hbs', handlebars({
-    layoutsDir: `${__dirname}/views/layouts`,
-    defaultLayout: 'main',
-    partialsDir: `${__dirname}/views/partials`,
-    extname: 'hbs'
-}));
 
 
 // Routes
@@ -69,31 +61,14 @@ app.get('/', (req, res) => {
     res.render('svgs');
 });
 
-app.get('/colors', (req, res) => {
-    res.render('colors');
-});
-
-app.get('/links', (req, res) => {
-    res.render('links');
-});
-
-app.get('/git', (req, res) => {
-    res.render('git');
-});
 
 app.get('/load/svgs', (req, res) => {
-    res.status(200);
-    res.json(svgList);
-});
-
-
-// Custom 404 page
-//####################################
-app.use(function(req, res) {
-    res.render('404');
+    res.status(200).json(svgList);
 });
 
 
 // Starting server on port
 //####################################
-app.listen(port, () => console.log(`Up and running: http://localhost:${port}/`));
+app.listen(port, () => {
+    console.log(`Up and running on: http://localhost:${port}/`);
+});
--- resources/js/svg.js
+++ resources/js/svg.js
@@ -1,47 +1,31 @@
-const url = new URL(window.location.href);
-
-if(url.pathname == "/"){
-    $.get('http://localhost:3001/load/svgs', function(svgList){
-        if(svgList.length > 0){        
-            for(var i=0; i<svgList.length; i++){
-                $(".svgContainer").append(`<h2>${svgList[i].directory}</h2>`);
-        
-                for(var n=0; n<svgList[i].svgs.length; n++){
-                    const colors = [
-                        '#35bf2f',
-                        '#5979da',
-                        '#f71828',
-                        '#fc5b1f',
-                        '#be42ff',
-                        '#be4c57',
-                        '#360cd9',
-                        '#34b0f0'
-                    ];
-                    var color = colors[Math.floor(Math.random()*colors.length)];
-                    $(".svgContainer").append(`
-                        <a style="color:${color};" id="${svgList[i].svgs[n].name}">
-                            ${svgList[i].svgs[n].code}
-                        </a>
-                    `);
-                }
-            };
-        } else {
-            $(".svgContainer").append(`
-                <div class="error">
-                    <h2>An error occured!</h2>
-                    <p>
-                        Either there are no folders with svgs to load or there
-                        <br>was an error fetching json data from the api
-                    </p>
-                </div>
-            `);
-        }
+$.get(`${window.location.href}load/svgs`, function(svgList){
+    if(svgList.length > 0){
+        for(var i=0; i<svgList.length; i++){
+            $(".svgContainer").append(`<h2>${svgList[i].directory}</h2>`);
     
-        $("svg").click(function(){
-            togglePopup($(this)[0]);
-        });
+            for(var n=0; n<svgList[i].svgs.length; n++){
+                $(".svgContainer").append(`
+                    <a style="color:#615f68;" id="${svgList[i].svgs[n].name}">
+                        ${svgList[i].svgs[n].code}
+                    </a>
+                `);
+            }
+        };
+    } else {
+        $(".svgContainer").append(`
+            <div class="error">
+                <h2>ERROR</h2>
+                <p>
+                    No svgs to load!
+                </p>
+            </div>
+        `);
+    }
+
+    $("svg").click(function(){
+        togglePopup($(this)[0]);
     });
-}
+});
 
 function togglePopup(code){
     // Visar / gömmer popup
